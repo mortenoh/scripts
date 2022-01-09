abec007717e06f8a87bfbe7707e3d411685b7d09
Morten Hansen <morten@winterop.com>
2022-01-09 16:01:45 +0700

    refactor: move into scripts folder

---
+++ scripts/apt-upgrade.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+APT_OPTIONS="-qq -o=Dpkg::Use-Pty=0 -y"
+
+sudo echo "Starting upgrade of server + lxc hosts"
+
+sudo apt $APT_OPTIONS update &> /dev/null
+sudo apt $APT_OPTIONS upgrade
+sudo apt $APT_OPTIONS autoremove &> /dev/null
+
+for x in $(lxc list volatile.last_state.power=RUNNING -cn --format csv);
+do
+    echo Updating LXC host $x
+    lxc exec $x -- apt $APT_OPTIONS update &> /dev/null
+    lxc exec $x -- apt $APT_OPTIONS upgrade
+    lxc exec $x -- apt $APT_OPTIONS autoremove &> /dev/null
+done
